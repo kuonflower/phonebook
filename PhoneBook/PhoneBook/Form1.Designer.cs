a4c7c137b460c53fcf2b3f9a9b0d3f0b216e6f46
kuonflower <68059211+kuonflower@users.noreply.github.com>
2020-10-22 21:59:33 +0900

    Add files via upload

---
+++ PhoneBook/PhoneBook/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PhoneBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nameList = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.phoneNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // nameList
+            // 
+            this.nameList.FormattingEnabled = true;
+            this.nameList.ItemHeight = 12;
+            this.nameList.Location = new System.Drawing.Point(24, 92);
+            this.nameList.Name = "nameList";
+            this.nameList.Size = new System.Drawing.Size(153, 136);
+            this.nameList.TabIndex = 0;
+            this.nameList.SelectedIndexChanged += new System.EventHandler(this.nameSelected);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "名前";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "電話番号";
+            // 
+            // phoneNumber
+            // 
+            this.phoneNumber.Enabled = false;
+            this.phoneNumber.Location = new System.Drawing.Point(200, 92);
+            this.phoneNumber.Name = "phoneNumber";
+            this.phoneNumber.Size = new System.Drawing.Size(100, 19);
+            this.phoneNumber.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 324);
+            this.Controls.Add(this.phoneNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nameList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox nameList;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox phoneNumber;
+    }
+}
+
---
+++ PhoneBook/PhoneBook/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneBook
+{
+    public partial class Form1 : Form
+    {
+        Dictionary<string, string> phoneBook;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //電話帳に名前を登録する
+
+            this.phoneBook = new Dictionary<string, string>();
+
+            //ファイルからデータを読み込む
+            ReadFromFile();
+
+
+            this.phoneBook.Add("山田一郎", "xxx-1234-1111");
+            this.phoneBook.Add("山田二郎", "xxx-1234-2222");
+            this.phoneBook.Add("山田三郎", "xxx-1234-3333");
+            this.phoneBook.Add("山田四郎", "xxx-1234-4444");
+
+            foreach(KeyValuePair<string,string> data in phoneBook)
+            {
+                this.nameList.Items.Add(data.Key);
+            }
+        }
+
+        private void ReadFromFile()
+        {
+            using(System.IO.StreamReader file = new System.IO.StreamReader(@"..\..\data.txt"))
+            {
+                while (!file.EndOfStream)
+                {
+                    string line = file.ReadLine();
+                    string[] data = line.Split(',');
+                    this.phoneBook.Add(data[0], data[1]);
+                }
+            }
+
+        }
+
+        private void nameSelected(object sender, EventArgs e)
+        {
+            string name = this.nameList.Text;
+            this.phoneNumber.Text = this.phoneBook[name];
+        }
+    }
+}
